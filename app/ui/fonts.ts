eee1ec235c19b20e39c01e57672363eaaf6597e4
supercoderl <150410325+supercoderl@users.noreply.github.com>
2024-06-08 10:09:55 +0700

    first

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+});
