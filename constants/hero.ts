7f68e4021a5a12aaeebeace139aad8e52a4e09e2
supercoderl <150410325+supercoderl@users.noreply.github.com>
2024-07-07 10:31:39 +0700

    fix-uppercase

--- constants/hero.ts
+++ constants/hero.ts
@@ -11,40 +11,40 @@ export const heros: Hero[] = [
         img_des: "",
         chap_num: 24,
         title: "My hero academia",
-        types: ["sub", "sub", "sub"],
-        des: "asdasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasdasdasdasdasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssasdasd"
+        types: ["Siêu anh hùng", "Khoa học viễn tưởng", "Hành động"],
+        des: "Theo dõi cuộc sống của cậu bé không có quirk Izuku Midoriya và cách anh trở thành anh hùng vĩ đại nhất. Midoriya, một cậu bé đã ngưỡng mộ các anh hùng và cuộc phiêu lưu của họ từ ngày anh được sinh ra, đến một thế giới không có quirk, nơi gần như mọi người sinh ra đều có một."
     },
     {
         img: fern,
         img_des: "",
-        chap_num: 24,
-        title: "My hero academia",
-        types: ["sub", "sub", "sub"],
-        des: "asdasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasdasdasdasdasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssasdasd"
+        chap_num: 16,
+        title: "Frieren - Pháp sư tiễn táng",
+        types: ["Kỳ ảo", "Phiêu lưu", "Chính kịch"],
+        des: "Fern là trẻ mồ côi mất cha mẹ trong chiến tranh ở đất nước phía Nam, định tự tử bằng cách nhảy khỏi khe núi khi Heiter cứu cô và được Heiter nhận nuôi."
     },
     {
         img: spy,
         img_des: "",
-        chap_num: 24,
-        title: "My hero academia",
-        types: ["sub", "sub", "sub"],
-        des: "asdasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasdasdasdasdasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssasdasd"
+        chap_num: 34,
+        title: "Spy × Family",
+        types: ["Hành động"],
+        des: "Spy x Family là một bộ truyện tranh hài hước và cảm động xoay quanh một gia đình trong thời kỳ Chiến Tranh Lạnh. Loid Forger, một điệp viên, nhận nuôi một cô bé là một phần của nhiệm vụ của mình. Để duy trì nhiệm vụ bí mật, Loid kết hôn với Yor, một sát thủ, và nhận nuôi Anya, một cô bé có khả năng đọc suy nghĩ."
     },
     {
         img: shinobu,
         img_des: "",
-        chap_num: 24,
-        title: "My hero academia",
-        types: ["sub", "sub", "sub"],
-        des: "asdasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasdasdasdasdasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssasdasd"
+        chap_num: 50,
+        title: "Kimetsu no Yaiba",
+        types: ["Tiểu thuyết phiêu lưu", "Kỳ ảo đen tối", "Võ thuật"],
+        des: "Kimetsu no Yaiba kể câu chuyện về cậu bé Tanjirou sau khi trải qua nỗi đau cả nhà bị quỷ sát hại, em gái Nezuko bị biến thành quỷ, đã lên đường để học hỏi và chiến đấu, trở thành một người diệt quỷ và tìm cách giúp Nezuko trở lại làm người."
     },
     {
         img: maho,
         img_des: "",
-        chap_num: 24,
-        title: "My hero academia",
-        types: ["sub", "sub", "sub"],
-        des: "asdasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasdasdasdasdasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssasdasd"
+        chap_num: 21,
+        title: "Mahou Shoujo Ni Akogarete",
+        types: ["Hài", "Ma pháp thiếu nữ", "Yuri"],
+        des: "Một cô gái rất ngưỡng mộ và muốn trở thành Mahou Shoujo. Tuy nhiên khi ước mơ trở thành sự thật, cô nhận ra... đời không như là mơ."
     },
 ]
 
